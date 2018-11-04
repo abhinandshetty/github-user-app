f66d29bf255fe7f5df06d979486253ec68a82849
Abhinand Shetty <abhinandshetty.13@gmail.com>
2018-11-04 19:14:39 +0530

    committing missing file: Repositories.js

---
+++ src/components/Repositories.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Repositories extends Component {
+    constructor(props) {
+        super(props);
+    }
+    renderUserRepositories =()=>{
+        return this.props.repos.map((repo,i) => (
+            <div className='respository'>
+                <div className="row ml-3">
+                    <div className="col-md-3"/>
+                    <div className="col-md-3">Repository {i+1}</div>
+                    <div className="col-md-6">{repo.name}</div>
+                </div>
+                <hr/>
+            </div>
+        ));
+    };
+    render() {
+        return (
+                <div>
+                    {this.renderUserRepositories()}
+                </div>
+        );
+    }
+}
+
+export default Repositories;
