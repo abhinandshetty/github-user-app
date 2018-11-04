cd29687aa143b4486a3ae201eb36880021e9494f
Abhinand Shetty <abhinandshetty.13@gmail.com>
2018-11-04 13:41:09 +0530

    1. Added call to fetch user repository information 2. Added api call to fetch user information on search -box keyup

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,23 +4,50 @@ class Users extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: []
+            users: [],
+            userRepo: []
         }
     }
 
     componentDidMount() {
+        this.getAllUsers();
+    }
+
+    getAllUsers = () => {
         fetch('https://api.github.com/users')
             .then(res => res.json())
             .then(data => this.setState({users: data}));
-    }
+    };
 
-    getUserInformation = (username, context) => {
+    getUserInformation = (username) => {
         fetch(`https://api.github.com/users/${username}`)
             .then(res => res.json())
-            .then(data => console.log(data));
-        };
+            .then(data => this.setState({users: [data]}));
+    };
+
+    getUserRepos = (username, context) => {
+        fetch(`https://api.github.com/users/${username}/repos`)
+            .then(res => res.json())
+            .then(data => this.setState({userRepo: data}));
+    };
+    searchKeyUp =(e)=>{
+        if(e.target.value) {
+            this.getUserInformation(e.target.value)
+        } else {
+            this.getAllUsers();
+        }
+    };
 
     render() {
+        const userRepo = this.state.userRepo.map((repo,i) => (
+            <div>
+                <div className="row">
+                    Repository {i+1} : {repo.name}
+                </div>
+                <hr/>
+            </div>
+
+        ));
         const users = this.state.users.map(user => (
             <div className="card m-3">
                 <div className="card-body">
@@ -32,19 +59,17 @@ class Users extends Component {
                                      className="image-cover"/>
                             </div>
                         </div>
-                        <div className="col-md-9">
+                        <div className="col-md-7">
                             <h5>{user.login}</h5>
                             <h6>Profile URL : {user.url}</h6>
                             <h6>Type : {user.type}</h6>
-                            <div className="row">
-                                <div className="col-md-3"/>
-                                <div className="col-md-3"/>
-                                <div className="col-md-3"/>
-                                <div className="col-md-3">
-                                    <button className="btn" onClick={this.getUserInformation.bind(this, user.login)}>Details</button>
-                                </div>
-                            </div>
                         </div>
+                        <div className="col-md-2">
+                             <button className="btn" onClick={this.getUserRepos.bind(this, user.login)}>Details</button>
+                        </div>
+                    </div>
+                    <div className="repo-info">
+                        {userRepo}
                     </div>
                 </div>
             </div>
@@ -64,7 +89,7 @@ class Users extends Component {
                         </div>
                         <div className="col-lg-3">
                             <div className="float-right">
-                                <input type="text" className="form-control" placeholder="Search User"/>
+                                <input type="text" className="form-control" placeholder="Search User" onKeyUp={this.searchKeyUp}/>
                             </div>
                         </div>
                         <div className="col-lg-3"/>
